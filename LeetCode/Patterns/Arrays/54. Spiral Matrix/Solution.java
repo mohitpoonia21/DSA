8f243572a05be7cfe806b4e7be46b3ce393eaf02
mohitpoonia21 <mohitp9911@gmail.com>
2026-08-19 00:33:26 +0530

    Add Java Solution to Patterns/Arrays

---
+++ LeetCode/Patterns/Arrays/54. Spiral Matrix/Solution.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int startingRow = 0;
+        int endingRow= m-1;
+
+        int startingCol =0;
+        int endingCol = n-1;
+
+        while(startingRow <= endingRow && startingCol <= endingCol){
+            // LOOP 1 left se right chlega row wise
+
+            for(int col = startingCol;col<=endingCol;col++){
+                result.add(matrix[startingRow][col]);
+            }
+            startingRow++;
+
+            // LOOP 2 top se bottom chlega col wise
+
+            for(int row = startingRow;row<=endingRow;row++){
+                result.add(matrix[row][endingCol]);
+            }
+            endingCol--;
+
+            // LOOP 3 right se left chlega row wise
+
+            if(startingRow<=endingRow){
+                for(int col = endingCol; col>=startingCol;col--){
+                result.add(matrix[endingRow][col]);
+            }
+            endingRow--;
+            }
+
+            // LOOP 4 bottom to top column wise
+
+            if(startingCol<=endingCol){
+                for(int row = endingRow;row>= startingRow;row--){
+                    result.add(matrix[row][startingCol]);
+                }
+                startingCol++;
+            }
+
+            
+        }
+        return result;
+        
+    }
+}
