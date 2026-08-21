5317953aeaaf297e4fee61ac4b9073cc64d50bcc
mohitpoonia21 <mohitp9911@gmail.com>
2026-08-22 01:35:21 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/977. Squares of a Sorted Array/Solution.java
+++ LeetCode/Patterns/Two Pointers/977. Squares of a Sorted Array/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         /*int n = nums.length;
 
         for(int i =0;i<n;i++){
-            nums[i] = nums[i]*nums[i];
+            nums[i] = nums[i]*nums[i];  //
         }
         Arrays.sort(nums);
         return nums;*/
