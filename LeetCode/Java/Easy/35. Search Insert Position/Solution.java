51135ca85d8c833194c4dce6af501cfefe94e0b0
mohitpoonia21 <mohitp9911@gmail.com>
2026-08-27 20:35:59 +0530

    Update Solution.java

--- LeetCode/Java/Easy/35. Search Insert Position/Solution.java
+++ LeetCode/Java/Easy/35. Search Insert Position/Solution.java
@@ -4,23 +4,20 @@ class Solution {
         int n = nums.length;
         int start = 0;
         int end = n-1;
-        int ans = -1;
 
         while(start<=end){
             int mid = start +(end-start)/2;
 
-            if(nums[mid]==target){
+            if(nums[mid]==target){ // agr mid target k equal hoga toh usse return krdenge
                 return mid;
             }
-            else if(target>nums[mid]){
-                ans = mid;
+            else if(target>nums[mid]){ // agr target mid se bda h toh right move krenge
                 start = mid+1;
             }
-            else{
-                ans = mid;
+            else{ // agr target mid se chhota h toh left move krenge 
                 end = mid-1;
             }
         }
-        return ans+1;    
+        return start;  // agr target nahi milta h toh start index p uss taget ki desired position hogi usse return krdenge  
     }
 }
