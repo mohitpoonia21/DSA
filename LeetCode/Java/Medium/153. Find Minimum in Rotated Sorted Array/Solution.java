7df0359526ed7824977e9643a7549b7476f9f052
mohitpoonia21 <mohitp9911@gmail.com>
2026-08-30 00:49:14 +0530

    Add Medium Java Solution for 153. Find Minimum in Rotated Sorted Array from LeetCode

--- LeetCode/Java/Medium/153. Find Minimum in Rotated Sorted Array/Solution.java
+++ LeetCode/Java/Medium/153. Find Minimum in Rotated Sorted Array/Solution.java
@@ -2,25 +2,20 @@ class Solution {
     public int findMin(int[] nums) {
 
         int n = nums.length; // array ki length
-
-        if(nums[0]<nums[n-1] || n==1){ // agr array rotated sorted na ho ya fir 1 hi element ho array mai toh array ka phla element hi sbse chhota hoga toh sidha usse hi return krdenge
-            return nums[0];
-        }
-
         int start = 0;
         int end = n-1;
 
-        while(start<=end){
+        while(start<end){
 
             int mid = start + (end-start)/2;
 
-            if(nums[mid]>=nums[n-1]){ // jbb array k mid ki value last value se badi ya barabar h mtlb L1 m h toh potenital answer ko store krlenge aur chceck krenge agr usse badi koi value bhi h toh islie right m move krenge
+            if(nums[mid]>nums[n-1]){ // jbb array k mid ki value last value se badi h mtlb L1 m h islie right m move krenge
                 start= mid+1;
             }
-            else{ //jbb array k mid ki value last value se chhoti h mtlb L2 m h toh left m move krenge
-                end = mid-1;
+            else{ //jbb array k mid ki value last value se chhoti ya barabr h mtlb L2 m h toh left m move krenge
+                end = mid;
             }
-       }
+        }
        return nums[start]; 
     }  
 }
