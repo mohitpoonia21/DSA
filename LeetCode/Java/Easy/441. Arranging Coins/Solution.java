aa532e532e34a75aaa5772e13d28fc23be5de0f7
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-03 22:51:25 +0530

    Add Easy Java Solution for 441. Arranging Coins from LeetCode

--- LeetCode/Java/Easy/441. Arranging Coins/Solution.java
+++ LeetCode/Java/Easy/441. Arranging Coins/Solution.java
@@ -7,16 +7,15 @@ class Solution {
         while(start<=end){
             int mid = start + (end-start)/2;
 
-            long coinsRequired = (long)mid * (mid+1)/2;
+            long coinsRequired = (long)mid * (mid+1)/2; // current mid k liye total kitne coins chiye staircase bnane k liye
 
-            if(coinsRequired<=n){
+            if(coinsRequired<=n){ // agr required coins given no se km h mtlb hoskta h aur bhi rows shyd add krskte h toh right move krke dekhenge
                 start =mid+1;
             }
-            else{
+            else{ // agr coins required given no se jyaada h toh left move krenge
                 end = mid-1;
             }
         }
-
         return start-1;
         
     }
