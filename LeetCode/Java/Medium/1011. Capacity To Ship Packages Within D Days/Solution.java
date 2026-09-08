d37af3dc9e0ccf2a7cd49c7ff5ba90a779a7453c
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-08 23:16:19 +0530

    Update Solution.java

--- LeetCode/Java/Medium/1011. Capacity To Ship Packages Within D Days/Solution.java
+++ LeetCode/Java/Medium/1011. Capacity To Ship Packages Within D Days/Solution.java
@@ -3,7 +3,7 @@ class Solution {
 
         int n = weights.length;
         int max =0;
-        for(int i =0;i<n;i++){
+        for(int i =0;i<n;i++){ // array ka max weight nikalenge 
             if(weights[i]>max){
                 max = weights[i];
             }
@@ -12,32 +12,32 @@ class Solution {
         int start = max;
         int sum=0;
 
-        for(int i =0;i<n;i++){
+        for(int i =0;i<n;i++){ // end m saare weights ka sum nikalenge agr 1 hi din m sbko ship krna hua toh saare package ek din m le jaane hoge toh capacity wahi hogi
             sum += weights[i];
         }
         int end = sum;
         int ans = -1;
 
         while(start<=end){
-            int mid = start + (end - start)/2;
-            int totalDays = 1;
+            int mid = start + (end - start)/2; // current capacity
+            int totalDays = 1; 
             int currentWeight = 0;
 
             for(int i = 0;i<n;i++){
 
-                if(weights[i]+currentWeight<=mid){
+                if(weights[i]+currentWeight<=mid){ //jitna hoskta h 1 din m utna allot krdenge
                     currentWeight+= weights[i];
                 }
-                else{
+                else{ // agr nahi hoskta toh days ko bdhyange aur jiss weight pr h usko current weight bna denge taaki agli br se wahi weight allot ho next day p
                     totalDays++;
                     currentWeight = weights[i];
                 }
             }
-            if(totalDays<=days){
+            if(totalDays<=days){ // iska mtlb h current capacity m sbb package ship hojaynge toh answer ko store krlenge aur left move krke dekhenge agr isse km capacity m bhi saare package ship hoskte h kya 
                 ans = mid;
                 end = mid-1;
             }
-            else{
+            else{ // iska mtlb iss capacity m saare package ship nhi hoskte islie right move krenge aur capacity ko increase krenge
                 start = mid+1;
             }
         }
