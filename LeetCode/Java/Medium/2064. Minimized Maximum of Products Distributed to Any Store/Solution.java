9293bf19449ccc3f5a459aa455c2d6b0a7c54563
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-11 21:30:54 +0530

    Update Solution.java

--- LeetCode/Java/Medium/2064. Minimized Maximum of Products Distributed to Any Store/Solution.java
+++ LeetCode/Java/Medium/2064. Minimized Maximum of Products Distributed to Any Store/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
         int len = quantities.length;
 
-        int start = 1;
+        int start = 1; // minimum 1 product toh hoga hi per store
 
         int max = 0;
         for(int i = 0;i<len;i++){
@@ -11,25 +11,25 @@ class Solution {
             }
         }
 
-        int end = max;
+        int end = max; // jbb n 1 hoga toh max quantity ussi m store hogi
         int ans =-1;
 
         while(start<=end){
-            int mid = start + (end-start)/2;
+            int mid = start + (end-start)/2; //  maximum capacity per store
 
             int storesRequired = 0;
 
-            for(int i =0;i<len;i++){
-                storesRequired += (quantities[i] + mid-1)/mid;
-                if(storesRequired>n){
+            for(int i =0;i<len;i++){ // kitne stores lgenge har product k liye agr mid capacity se store krenge
+                storesRequired += (quantities[i] + mid-1)/mid; // ceil division 
+                if(storesRequired>n){ // agr storesRequired jyaada h n se mtlb current capacity enough nahi h products ko store krne k liye
                     break;
                 }
             }
-            if(storesRequired<=n){
+            if(storesRequired<=n){ // agr stores required km ya barabr h n se mtlb current capacity se sbb products ko store krpaynge islie current ans ko store krlenge aur left move krke dekhenge agr isse km capacity m bhi products ko store krpaynge kya
                 ans = mid;
                 end = mid-1;
             }
-            else{
+            else{ // agr storesRequired jyaada h n se mtlb curent capacity se products ko store nahi krpaaynge islie right move krenge
                 start = mid+1;
             }
         }
