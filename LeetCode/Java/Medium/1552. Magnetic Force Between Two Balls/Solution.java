4d85258db4b3e95e58c9e6af52f5b731d6941303
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-12 23:48:28 +0530

    Update Solution.java

--- LeetCode/Java/Medium/1552. Magnetic Force Between Two Balls/Solution.java
+++ LeetCode/Java/Medium/1552. Magnetic Force Between Two Balls/Solution.java
@@ -4,31 +4,31 @@ class Solution {
 
         int n = position.length;
 
-        int start =1;
-        int end = position[n-1] - position[0];
+        int start =1; // // start ko 1 rakhenge kyunki positions distinct hain, toh minimum possible distance 1 hoga
+        int end = position[n-1] - position[0]; // end ko last - first rkhenge agr 1 ball 1st position p hua aur dusra last position p toh wahi max possible min h
         int ans = -1;
 
 
         while(start<=end){
-            int mid = start + (end - start)/2;
-            int balls = 1;
+            int mid = start + (end - start)/2; // minimum distance jisko abhi test kr rhe h
+            int balls = 1; // 1st ball ko aate hi 0 index p place krdenge
             int lastPosition = 0;
 
-            for(int i =1;i<n;i++){
-                if(position[i]-position[lastPosition]>=mid){
-                    lastPosition = i;
+            for(int i =1;i<n;i++){ // 1 se start krenge becuase 0 index p alr 1 ball placed h
+                if(position[i]-position[lastPosition]>=mid){ // agr current - last ka distance minimum distance se bada ya barabr hua toh udhr ball place krdenge
+                    lastPosition = i; // abb  current position last position bnjaygi
                     balls++; 
                 }
-                else{
+                else{ // agr current - last chhota hua minimum distance se toh skip krdenge
                     continue;
                 }
 
             }
-            if(balls>=m){
+            if(balls>=m){ // agr balls jyaada ya barabr huyi m se toh iss minimum distance(mid) m saari balls place hogyi toh iss possible answer ko store krlenge aur right move krenge aur dekhenge agr isse jyaada minimum distance m saari balls place hogi kya kuki hume maximum of min distance chiye
                 ans = mid;
                 start = mid+1;
             }
-            else{
+            else{ // agr saari balls place nahi hogi toh minmum distance jyaada h isme saari balls place nahi hopayi islie left move krenge 
                 end = mid-1;
             }
         }
