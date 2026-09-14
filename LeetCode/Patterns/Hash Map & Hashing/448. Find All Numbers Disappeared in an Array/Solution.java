65118ffe4c8850505c9325ddbb3872cce6702277
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-15 02:19:15 +0530

    Add Easy Java Solution for 448. Find All Numbers Disappeared in an Array from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/448. Find All Numbers Disappeared in an Array/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/448. Find All Numbers Disappeared in an Array/Solution.java
@@ -6,11 +6,12 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
 
-        for(int i =0;i<n;i++){
+
+        for(int i =0;i<n;i++){ // frequencies ko store krenge getOrDefault ko use krke agr nums[i] alr exist krta hoga toh freq ko +1 krdenge vrna freq 0+1 hojayga
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        for(int i = 1; i<=n;i++){
+        for(int i = 1; i<=n;i++){ // traverse krenge aur dekhenge agr 1 to n range mai koi number ka freq 0 h toh usko ans m add krdenge
             if(map.getOrDefault(i,0)==0){
                 ans.add(i);
             }
