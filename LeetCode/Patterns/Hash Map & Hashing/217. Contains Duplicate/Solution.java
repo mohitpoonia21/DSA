9063f2800ec87ae4437993ddd384eaf170bad996
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-15 23:25:16 +0530

    Add Easy Java Solution for 217. Contains Duplicate from LeetCode

--- LeetCode/Patterns/Hash Map & Hashing/217. Contains Duplicate/Solution.java
+++ LeetCode/Patterns/Hash Map & Hashing/217. Contains Duplicate/Solution.java
@@ -5,16 +5,16 @@ class Solution {
 
         HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;i++){ // frequency ko store krenge agr phle se vo number present hoga toh frequency ko +1 krdenge vrna 1 krenge
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        for(int i =1;i<=n;i++){
-            if(map.getOrDefault(i,0)>1){
+        for(int num : nums){ // firse traverse krenge aur dekhenge kisi bhi number ki freq agr >1 hogi toh true return krdenge
+            if(map.get(num)>1){
                 return true;
             }
         }
-        return false;
+        return false; //agr kisi bhi no ki frequency >1 nahi hogi toh false return krdenge
         
     }
 }
