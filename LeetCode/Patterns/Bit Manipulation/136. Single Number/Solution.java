0ce2f539768b440b3f2f18edd3dca1996da3d359
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-16 23:35:58 +0530

    Add Easy Java Solution for 136. Single Number from LeetCode

---
+++ LeetCode/Patterns/Bit Manipulation/136. Single Number/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int n = nums.length;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(int num : nums){
+            if(map.get(num)==1){
+                return num;
+            }
+        }
+        return -1;
+        
+    }
+}
