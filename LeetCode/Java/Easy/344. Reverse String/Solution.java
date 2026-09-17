dddf21154172355d215a9eaa7584b47cfc537c83
mohitpoonia21 <mohitp9911@gmail.com>
2026-09-17 23:55:27 +0530

    Add Easy Java Solution for 344. Reverse String from LeetCode

--- LeetCode/Java/Easy/344. Reverse String/Solution.java
+++ LeetCode/Java/Easy/344. Reverse String/Solution.java
@@ -4,14 +4,13 @@ class Solution {
         int left = 0;
         int right = s.length -1;
 
-        while(left<=right){
+        while(left<=right){ // jbb tk left right se chhota ya barabr h in place swapping krenge temp variable ka use krke temp k andr left char ko store krlenge fir left m right wale ko place krdenge abb left ki value lost nahi because alr temp m stored h islie right m abb temp ki value ko daal denge
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
 
-            left++;
-            right--;
+            left++; // left ko aage bdhaynge
+            right--; // right ko picche krenge
         }
-        
     }
 }
